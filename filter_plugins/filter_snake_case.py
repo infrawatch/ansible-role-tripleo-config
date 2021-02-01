79fa19413a08a7bef0b66d2d7a37b5c3cb573742
Emma Foley <efoley@redhat.com>
2021-02-01 14:03:20 +0000

    Fix feedback from the tripleo_ansible linter

--- filter_plugins/filter_snake_case.py
+++ filter_plugins/filter_snake_case.py
@@ -1,10 +1,14 @@
-# copied from https://github.com/infrawatch/tripleo-qdr-ansible-role/commit/4897cb6a09fd83c12b8d7a578c845471afe68dbc
+# copied from
+# https://github.com/infrawatch/tripleo-qdr-ansible-role/commit/4897cb6a09fd83c12b8d7a578c845471afe68dbc
 import re
 
+
 def filter_snake_case(camel_case_string):
-  name = re.sub(r'(.)([A-Z][a-z]+)', r'\1_\2', camel_case_string)
-  return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', camel_case_string).lower()
+    camel_case_string = re.sub(r'(.)([A-Z][a-z]+)', r'\1_\2',
+                               camel_case_string)
+    return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', camel_case_string).lower()
+
 
 class FilterModule(object):
     def filters(self):
-      return {'snake_case': filter_snake_case}
+        return {'snake_case': filter_snake_case}
